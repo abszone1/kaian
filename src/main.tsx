737041d9e27c4c827087b4a496b1fb03d5270d44
Name <abszone6@gmail.com>
2025-01-18 04:45:54 +0200

    00

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,16 @@ import { RouterProvider } from 'react-router'
 import { router } from './routes'
 import { IntlProvider } from 'react-intl'
 
-function loadLocaleData(locale: string){
-  switch (locale) {
-    case "ar":
-      return import('../compiled-lang/ar.json')
-    case "en":
-      return import('../compiled-lang/en.json')
-    default:
-      return import('../compiled-lang/en.json')
-  }
-}
+// function loadLocaleData(locale: string){
+//   switch (locale) {
+//     case "ar":
+//       return import('../compiled-lang/ar.json')
+//     case "en":
+//       return import('../compiled-lang/en.json')
+//     default:
+//       return import('../compiled-lang/en.json')
+//   }
+// }
 
 
 createRoot(document.getElementById('root')!).render(
